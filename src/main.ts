1a665aee0b4bb854a9422b4d04268eb338a03bb2
drumath <2237k.drumath@gmail.com>
2023-04-30 00:01:11 +0900

    :sparkles: cubeを出すまで

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,29 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.scss";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { Engine, MeshBuilder, Scene } from "@babylonjs/core";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const main = async () => {
+  const renderCanvas = document.getElementById(
+    "renderCanvas"
+  ) as HTMLCanvasElement;
+  if (!renderCanvas) {
+    return;
+  }
+
+  renderCanvas.width = document.firstElementChild?.clientWidth ?? 0;
+  renderCanvas.height = document.firstElementChild?.clientHeight ?? 0;
+
+  const engine = new Engine(renderCanvas, true);
+  const scene = new Scene(engine);
+
+  scene.createDefaultCameraOrLight(true, true, true);
+
+  MeshBuilder.CreateBox("box", { size: 0.1 }, scene);
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
+};
+
+main();
+
