109110bd577c0e01319d233756a7bbda1360638f
drumath <2237k.drumath@gmail.com>
2023-04-30 00:38:40 +0900

    :sparkles: 6枚同時の画像トラッキング実装

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import "./style.scss";
 
-import { Engine, MeshBuilder, Scene } from "@babylonjs/core";
+import {
+  Engine,
+  IWebXRImageTrackingOptions,
+  MeshBuilder,
+  Quaternion,
+  Scene,
+  TransformNode,
+  WebXRFeatureName,
+  WebXRImageTracking,
+} from "@babylonjs/core";
+
+type TrackableObjectDictionary = {
+  [id: number]: TransformNode;
+};
 
 const main = async () => {
   const renderCanvas = document.getElementById(
@@ -18,7 +31,69 @@ const main = async () => {
 
   scene.createDefaultCameraOrLight(true, true, true);
 
-  MeshBuilder.CreateBox("box", { size: 0.1 }, scene);
+  const trackingCubeList: TrackableObjectDictionary = [];
+
+  const xr = await scene.createDefaultXRExperienceAsync({
+    uiOptions: {
+      sessionMode: "immersive-ar",
+      referenceSpaceType: "unbounded",
+    },
+  });
+  const featureManager = xr.baseExperience.featuresManager;
+
+  const imageTracking = featureManager.enableFeature(
+    WebXRFeatureName.IMAGE_TRACKING,
+    "latest",
+    {
+      images: [
+        {
+          src: "https://www.oreilly.co.jp/books/images/picture_large978-4-87311-565-8.jpeg",
+          estimatedRealWorldWidth: 0.05,
+        },
+        {
+          src: "https://www.oreilly.co.jp/books/images/picture_large978-4-8144-0001-0.jpeg",
+          estimatedRealWorldWidth: 0.05,
+        },
+        {
+          src: "https://www.oreilly.co.jp/books/images/picture_large978-4-8144-0002-7.jpeg",
+          estimatedRealWorldWidth: 0.05,
+        },
+        {
+          src: "https://www.oreilly.co.jp/books/images/picture_large978-4-8144-0008-9.jpeg",
+          estimatedRealWorldWidth: 0.05,
+        },
+        {
+          src: "https://www.oreilly.co.jp/books/images/picture_large978-4-8144-0018-8.jpeg",
+          estimatedRealWorldWidth: 0.05,
+        },
+        {
+          src: "https://www.oreilly.co.jp/books/images/picture_large4-87311-307-5.jpeg",
+          estimatedRealWorldWidth: 0.05,
+        },
+      ],
+    } as IWebXRImageTrackingOptions
+  ) as WebXRImageTracking;
+
+  imageTracking.onTrackableImageFoundObservable.add((event) => {
+    trackingCubeList[event.id] = MeshBuilder.CreateBox(
+      "trackable cube",
+      { size: 0.02 },
+      scene
+    );
+  });
+
+  imageTracking.onTrackedImageUpdatedObservable.add((event) => {
+    const trackingCube = trackingCubeList[event.id];
+    if (!trackingCube) {
+      return;
+    }
+
+    event.transformationMatrix.decompose(
+      trackingCube.scaling,
+      trackingCube.rotationQuaternion as Quaternion | undefined,
+      trackingCube.position
+    );
+  });
 
   engine.runRenderLoop(() => {
     scene.render();
